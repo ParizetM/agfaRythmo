0cadee0fe18f145196abcf82d996eba305a3b212
Parizetm <151616242+ParizetM@users.noreply.github.com>
2025-10-30 00:39:53 +0100

    Update cors.php

--- agfa-rythmo-backend/config/cors.php
+++ agfa-rythmo-backend/config/cors.php
@@ -1,23 +1,15 @@
 <?php
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'http://localhost:5173',
         'http://agfarythmo.agfagoofay.fr',
         'https://agfarythmo.agfagoofay.fr',
     ],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
